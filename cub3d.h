2eb77de159b8d61936bd7b6f78e0c5d1fafd69d4
Candrese <candrese@student.42Heilbronn.de>
2025-02-25 10:01:09 +0100

    current headerfile, with new raycasting testing functions  and building functions of our 2d map

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 13:19:49 by candrese          #+#    #+#             */
-/*   Updated: 2025/02/25 02:10:42 by candrese         ###   ########.fr       */
+/*   Updated: 2025/02/25 09:59:04 by candrese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,32 +32,38 @@
 
 typedef struct s_cub3d
 {
+	// mlx
 	mlx_t			*mlx;
 	mlx_image_t		*img;
+
+	// general
 	int				x;
 	int				y;
 	int				line_pos;
 	int				direction;
 	int				line_length;
-	int				r;
-	int				g;
-	int				b;
+	char			**test_map;
+	
+	// player position and direction
+	double			pos_x;
+	double			pos_y;
+	double			dir_x;
+	double			dir_y;
+	double			plane_x;
+	double			plane_y;
 }	t_cub3d;
-
-typedef struct s_pov
-{
-	int		x;
-	int		y;
-
-}	t_pov;
 
 void	clean_exit(t_cub3d *c);
 
 void	key_input(t_cub3d *c);
 
 void	main_loop(void *param);
-
 void	put_cub3d(t_cub3d *c);
 void	draw_line(t_cub3d *c, int x1, int y1, int x2, int y2, uint32_t color);
+
+char	**test_map(void);
+void	calculate_ray_direction(t_cub3d *c, int x, double *ray_dir_x, double *ray_dir_y);
+void	draw_minimap(t_cub3d *c);
+void	draw_rectangle(t_cub3d *c, int x, int y, int width, int height, uint32_t color);
 
 #endif
