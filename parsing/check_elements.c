f70adb98ba8f3e39b536ecd542fcdfd646b9a6eb
jidrizi <idrizijuxhin@gmail.com>
2025-03-11 19:15:21 +0100

    working on the elements

---
+++ parsing/check_elements.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_elements.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jidrizi <jidrizi@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 16:15:24 by jidrizi           #+#    #+#             */
+/*   Updated: 2025/03/11 19:11:53 by jidrizi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parse.h"
+
+//we set map to null so i can check out of this function if theres an error
+static void	handle_textures(char **map)
+{
+	char	*path;
+	int		i;
+	int		fd;
+
+	i = 0;
+	while (map[0][i] && map[0][i] != ' ' && map[0][i] != '\t'
+			&& map[0][i] != '\n')
+		i++;
+	path = ft_substr(map[0], 0, i);
+	fd = open(path, O_RDONLY);
+	ft_free_and_null((void **)&path);
+	if (fd < 0)
+		ft_free_and_null((void **)map);
+	close(fd);
+}
+
+static void	handle_element(char **map, t_elements *elements, int instance)
+{
+	if (instance == NO)
+		elements->no_count++;
+	else if (instance == SO)
+		elements->so_count++;
+	else if (instance == WE)
+		elements->we_count++;
+	else if (instance == EA)
+		elements->ea_count++;
+	else if (instance == F)
+		elements->f_count++;
+	else if (instance == C)
+		elements->c_count++;
+	if (instance == F || instance == C)
+		map[0] += 1;
+	else
+		map[0] += 2;
+	while (map[0][0] && (map[0][0] == ' ' || map[0][0] == '\t'
+			|| map[0][0] == '\n'))
+		map[0]++;
+	handle_textures(map);
+}
+
+int	check_elements(t_elements *elements, char *map)
+{
+	while (map && map[0])
+	{
+		if (map[0] == 'N' && map[1] == 'O')
+			handle_element(&map, elements, NO);
+		else if (map[0] == 'S' && map[1] == 'O')
+			handle_element(&map, elements, SO);
+		else if (map[0] == 'W' && map[1] == 'E')
+			handle_element(&map, elements, WE);
+		else if (map[0] == 'E' && map[1] == 'A')
+			handle_element(&map, elements, EA);
+		else if (map[0] == 'F')
+			handle_element(&map, elements, F);
+		else if (map[0] == 'C')
+			handle_element(&map, elements, C);
+		if (map)
+			map += 1;
+	}
+	if (elements->no_count != 1 || elements->so_count != 1
+		|| elements->we_count != 1 || elements->ea_count != 1
+		|| elements->f_count != 1 || elements->c_count != 1
+		|| !map)
+			return (error_msg("Something wrong with elements\n"), EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
--- parsing/parse.c
+++ parsing/parse.c
@@ -6,35 +6,40 @@
 /*   By: jidrizi <jidrizi@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 18:06:32 by jidrizi           #+#    #+#             */
-/*   Updated: 2025/03/08 17:05:25 by jidrizi          ###   ########.fr       */
+/*   Updated: 2025/03/11 19:00:59 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 
-static int	check_cub(char *map_cub)
+static int	check_cub(char *map_dot_cub)
 {
 	int	len;
 	int	dif;
 
-	len = ft_strlen(map_cub);
+	len = ft_strlen(map_dot_cub);
 	len -= 4;
 	if (len < 0)
 		return (error_msg("File is not a .cub file\n"), EXIT_FAILURE);
-	dif = ft_strncmp(&map_cub[len], ".cub", 4);
+	dif = ft_strncmp(&map_dot_cub[len], ".cub", 4);
 	if (dif != 0)
 		return (error_msg("File is not a .cub file\n"), EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
 
-int parse(int argc, char **argv)
+int	parse(int argc, char **argv)
 {
-	char	*map[2];
+	static t_elements	elements;
+	char		*map;
 
 	if (argc != 2)
-		return (error_msg("Number of args is not correct\n"), EXIT_FAILURE);
+		return (error_msg("Number of arguments isn't correct\n"), EXIT_FAILURE);
 	if (check_cub(argv[1]) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	map = get_map_array(argv[1]);
-
+	map = get_map_whole(argv[1]);
+	if (!map)
+		return (EXIT_FAILURE);
+	if (check_elements(&elements, map) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
